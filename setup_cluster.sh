2f05d18cd185a716b84fa8a31c6de5197a642665
jeff.vincent <jeff.d.vincent@gmail.com>
2023-12-14 19:01:41 -0800

    Add ingress controller check

--- setup_cluster.sh
+++ setup_cluster.sh
@@ -7,13 +7,34 @@ echo "================================================"
 echo "Waiting for Nginx ingress controller to spin up."
 echo "================================================"
 
+# Wait for the "controller" pod to be in a "Running" state
+pod_name=""
 for ((i=11; i>0; i--)); do
-    echo "Waiting for $((i * 10)) seconds..."
+    # Get the pod name containing "controller"
+    pod_name=$(kubectl get pods -n ingress-nginx | grep controller | awk '{print $1}')
+
+    # Check if the pod is running
+    if [ -n "$pod_name" ]; then
+        pod_status=$(kubectl get pod -n ingress-nginx "$pod_name" -o jsonpath='{.status.phase}')
+        if [ "$pod_status" == "Running" ]; then
+            echo "."
+            sleep 25
+            break
+        fi
+    fi
+
+    echo "."
     sleep 10
 done
+
+if [ -z "$pod_name" ]; then
+    echo "Failed to find a 'controller' pod in a 'Running' state within the timeout."
+    exit 1
+fi
 
 echo "================================================"
 echo "Deploying sample application."
 echo "================================================"
 
 kubectl apply -f k8s/app.yaml
+
