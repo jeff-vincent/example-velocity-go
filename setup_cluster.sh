87e53b74b9868e4369de78101d867e2b65415e7b
jeff.vincent <jeff.d.vincent@gmail.com>
2023-12-14 17:33:59 -0800

    Add sleep counter

--- setup_cluster.sh
+++ setup_cluster.sh
@@ -2,11 +2,18 @@
 
 kind create cluster --config=k8s/config.yaml
 kubectl apply -f k8s/kind-nginx.yaml
+
 echo "================================================"
 echo "Waiting for Nginx ingress controller to spin up."
 echo "================================================"
-sleep 120
+
+for ((i=11; i>0; i--)); do
+    echo "Waiting for $((i * 10)) seconds..."
+    sleep 10
+done
+
 echo "================================================"
 echo "Deploying sample application."
 echo "================================================"
+
 kubectl apply -f k8s/app.yaml
